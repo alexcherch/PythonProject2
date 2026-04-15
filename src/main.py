a709be79b6e4d75430b54b1e23b35b1eef468c71
Alexander Potashev <alexcherch0@gmail.com>
2026-04-15 19:53:14 +0300

    new function and test

--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
+import math
+
+
 def divide(a, b):
     if b > 0:
         return a / b
     return 0
 
+
+def calculate_logarithm(x, base):
+    return math.log(x, base)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,6 @@
-from src.main import divide
+import pytest
+
+from src.main import divide, calculate_logarithm
 
 
 def test_divide():
@@ -6,3 +8,12 @@ def test_divide():
 
     assert divide (2, 0) == 0
 
+def test_calc_log():
+    assert calculate_logarithm(8, 2) == 3.0
+    assert calculate_logarithm(8, 4) == 1.5
+
+    with pytest.raises(ValueError):
+        calculate_logarithm(0, 2)
+
+    with pytest.raises(ValueError):
+        calculate_logarithm(8, 0)
